90ae89f69d918da1321dee10af9818d092c3693c
Filip Ghimpeteanu <fghimpeteanu@endava.com>
2016-07-06 16:52:49 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // TODO Auto-generated method stub
+	    System.out.println("Hello Everyone");
+	    anotherMetod();
+	}
+	public static void anotherMetod() {
+	    System.out.println("aaaa");
+	    List<Integer> list = new ArrayList();
+	    list.add(1);
+	    list.add(2);
+	    
+	    System.out.println(list);
+	}
+	
+	
+
+}
